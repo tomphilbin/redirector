570c011af267c37585512e7c0bb4c86a1029aa67
Tom Philbin <t.philbin@gmail.com>
2016-01-30 12:43:25 +0000

    Added openshift port environment variable.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.get('/github', function(req, res) {
   res.redirect('https://github.com/tomphilbin');
@@ -9,4 +10,6 @@ app.get('/stackoverflow', function(req, res) {
   res.redirect('http://stackoverflow.com/users/1584074/tom-p');
 })
 
-app.listen(80);
+app.listen(port, function() {
+  console.log('Listening on port ' + port + '.');
+});
